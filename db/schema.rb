bbd74a55af520693aa1ce447c81b24f02ff0958f
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-03-24 18:07:21 +0100

    Events page style and footer correction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_110136) do
+ActiveRecord::Schema.define(version: 2020_03_24_143955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,9 +36,9 @@ ActiveRecord::Schema.define(version: 2020_03_23_110136) do
 
   create_table "events", force: :cascade do |t|
     t.text "description"
-    t.time "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "time"
   end
 
 end
