af6ac389df8756734536c9e31a2a8cc407ef7eb3
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-04-02 15:44:16 +0200

    no admin panel yet fix dropdown

--- app/javascript/components/modal.js
+++ app/javascript/components/modal.js
@@ -2,8 +2,6 @@ const initModalOnClick = () => {
   const modal = document.querySelector('.smodal');
   const dropdown = document.getElementById('dropdown');
   const close = document.querySelector('.close');
-  const menu = document.querySelector('.links');
-  const links = menu.querySelectorAll('a');
   dropdown.addEventListener('click', (event) => {
     modal.style.display = 'flex';
   });
