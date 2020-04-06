a40cd0264a46b031782c3da40944455f2e0472a0
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-04-06 18:38:38 +0200

    swiper fix

--- app/javascript/components/swiper.js
+++ app/javascript/components/swiper.js
@@ -3,7 +3,11 @@ import Swiper from 'swiper';
 const initSwiper = () => {
   var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
-
+    speed: 1000,
+    autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
