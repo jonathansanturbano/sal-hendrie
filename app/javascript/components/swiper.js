48fa10ebea2cf2cd27e5cd6b4d1cbf9fe1fd1a5c
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-04-11 16:27:34 +0200

    responsive home and fanart

--- app/javascript/components/swiper.js
+++ app/javascript/components/swiper.js
@@ -1,29 +1,57 @@
 import Swiper from 'swiper';
 
 const initSwiper = () => {
-  var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    speed: 1000,
-    loop: true,
-    roundLengths: true,
-    spaceBetween: 200,
-    autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-          },
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+  if (document.querySelector('.fanart-author')) {
+    var fanSwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+      speed: 1000,
+      loop: true,
+      roundLengths: true,
+      spaceBetween: 200,
+      autoplay: {
+              delay: 2500,
+              disableOnInteraction: false,
+            },
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
 
-    // Navigation arrows
-    // navigation: {
-    //   nextEl: '.swiper-button-next',
-    //   prevEl: '.swiper-button-prev',
-    // },
+      // Navigation arrows
+      // navigation: {
+      //   nextEl: '.swiper-button-next',
+      //   prevEl: '.swiper-button-prev',
+      // },
 
-    // And if we need scrollbar
-  })
+      // And if we need scrollbar
+    })
+  }
+  if (document.querySelector('.button').textContent === 'ORDER SIGNED BOOKS') {
+    var homeSwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+      speed: 1000,
+      loop: true,
+      roundLengths: true,
+      spaceBetween: 200,
+      autoplay: {
+              delay: 2500,
+              disableOnInteraction: false,
+            },
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      // navigation: {
+      //   nextEl: '.swiper-button-next',
+      //   prevEl: '.swiper-button-prev',
+      // },
+
+      // And if we need scrollbar
+    })
+  }
 }
 
 export { initSwiper };
