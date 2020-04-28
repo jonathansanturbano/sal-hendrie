214e939e41356711c06d7a84f41ed39b21da6726
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-04-28 12:15:03 +0200

    book show and bio complete minus toggle for signed book

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/bio', to: 'pages#bio', as: 'bio'
   # resources :events, only: [:index]
-  resources :books, only: [:index]
+  resources :books, only: [:index, :show]
 
   resources :contacts, only: [:new, :create]
   # resources :blogs, only: [:index, :show]
